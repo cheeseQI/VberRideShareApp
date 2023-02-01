227e0fb99523c1cc2d7928099a37e4c406e57fea
sq44 <sq44@duke.edu>
2023-02-01 10:54:38 -0500

    modify user_table with email column

--- web-app/vber/models.py
+++ web-app/vber/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(models.Model):
     user_name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
     is_driver = models.BooleanField(default=False)
     is_login = models.BooleanField(default=False)
 
@@ -21,7 +22,6 @@ class Vehicle(models.Model):
 class Ride(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ride_as_owner')
     sharer = models.ManyToManyField(User, related_name='ride_as_sharer', blank=True)
-    is_login = models.BooleanField(default=False)
     can_share = models.BooleanField(default=False)
     dest_addr = models.CharField(max_length=50)
     required_time = models.DateTimeField()
