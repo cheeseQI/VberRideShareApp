ff898966326b230774331dfb6f2bf17661195c96
sq44 <sq44@duke.edu>
2023-02-04 00:31:31 -0500

    feat: Ride Searching for Sharer complete filter and query part, need to deal with passing party number and add to db next

--- web-app/vber/urls.py
+++ web-app/vber/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('driver_view/<int:ride_id>/mark_status_complete', views.mark_complete_by_driver, name='mark_status_complete'),
     path('driver_search', views.ride_search_by_driver, name='driver_search'),
     path('driver_search/<int:ride_id>/mark_status_confirmed', views.mark_confirmed_by_driver, name='mark_status_confirmed'),
+    path('sharer_search', views.ride_search_by_sharer, name='sharer_search'),
+    path('sharer_search_result', views.show_ride_search_result_by_sharer, name='sharer_search_result'),
+    path('sharer_search_result/<int:ride_id>/join_ride_by_sharer', views.join_ride_by_sharer, name='join_ride')
 ]
--- web-app/vber/views.py
+++ web-app/vber/views.py
@@ -12,7 +12,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the vber index.")
 
 
-# the driver can search for open ride that fit vehicle and passenger information
+# the driver can search for open ride that fit vehicle and passenger number
 def ride_search_by_driver(request):
     vehicle = get_vehicle_by_driver(request)
     if vehicle is None:
@@ -23,6 +23,33 @@ def ride_search_by_driver(request):
     ride_list = Ride.objects.filter(id__in=r_ids)
     context = {'ride_list': ride_list}
     return render(request, 'vber/driver_search.html', context)
+
+
+# the sharer can search for open ride that fit destination, arrival time window, and passenger number
+def ride_search_by_sharer(request):
+    return render(request, 'vber/sharer_search.html')
+
+
+# the sharer can search for open ride that fit destination, arrival time window, and passenger number
+def show_ride_search_result_by_sharer(request):
+    dest = request.POST.get('dest')
+    e_time = request.POST.get('earliest_arrival_time')
+    l_time = request.POST.get('latest_arrival_time')
+    ride_list = list(Ride.objects.filter(status='open', dest_addr=dest, required_time__range=(e_time, l_time)))
+    number_in_party = request.POST.get('number_in_party')
+    context = {'ride_list': ride_list, 'number_in_party': number_in_party}
+    return render(request, 'vber/sharer_search_result.html', context)
+
+
+def join_ride_by_sharer(request, ride_id, number_in_party):
+    ride = Ride.objects.get(id=ride_id)
+    # todo: only user for test!
+    request.session["username"] = "test_user"
+    curr_user = User.objects.get(user_name=request.session["username"])
+    # todo: add sharer and add party here
+    # ride.sharer.add(curr_user)
+    # ride.number_in_party.add({})
+    return HttpResponseRedirect(reverse('vber:sharer_search'))
 
 
 # the driver can view the information of ride, including owner, sharer and their parties.
@@ -36,17 +63,6 @@ def ride_view_by_driver(request):
     return render(request, 'vber/driver_view.html', context)
 
 
-# helper to get driver's vehicle id, todo:  to a util.py package
-def get_vehicle_by_driver(request):
-    # user for test!
-    request.session["username"] = "test_user"
-    # todo: need to verify the user valid or not first
-    curr_user = User.objects.get(user_name=request.session["username"])
-    if not curr_user.is_driver:
-        return None
-    return Vehicle.objects.get(driver_id=curr_user.id)
-
-
 def mark_complete_by_driver(request, ride_id):
     ride = Ride.objects.get(id=ride_id)
     ride.status = 'complete'
@@ -54,13 +70,16 @@ def mark_complete_by_driver(request, ride_id):
     return HttpResponseRedirect(reverse('vber:driver_view'))
 
 
+# todo: abstract to util.py as a change_status(change_code, ride)
 def mark_confirmed_by_driver(request, ride_id):
     ride = Ride.objects.get(id=ride_id)
     ride.status = 'confirmed'
     ride.save()
     owner = ride.owner
-    share_list = ride.sharer
     send_mail_to_passenger(owner)
+    share_list = ride.sharer
+    for share in share_list.all():
+        send_mail_to_passenger(share)
     return HttpResponseRedirect(reverse('vber:driver_search'))
 
 
@@ -75,3 +94,15 @@ def send_mail_to_passenger(user):
         [user.email],
         fail_silently=False,
     )
+
+
+# helper to get driver's vehicle id, todo:  to a util.py package
+def get_vehicle_by_driver(request):
+    # todo: only user for test!
+    request.session["username"] = "test_user"
+    # todo: need to verify the user valid or not first
+    curr_user = User.objects.get(user_name=request.session["username"])
+    if not curr_user.is_driver:
+        return None
+    return Vehicle.objects.get(driver_id=curr_user.id)
+
